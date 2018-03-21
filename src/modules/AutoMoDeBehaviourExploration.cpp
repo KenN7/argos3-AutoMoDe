b0037817d30060b2f86b1e2893839c4a14e86255
Ken <ken@kenh.fr>
2018-03-21 20:32:42 +0100

    corrected epuck speed to 12, new BL false pos proba

--- src/modules/AutoMoDeBehaviourExploration.cpp
+++ src/modules/AutoMoDeBehaviourExploration.cpp
@@ -51,7 +51,7 @@ namespace argos {
 	void AutoMoDeBehaviourExploration::Init() {
 		m_unTurnSteps = 0;
 		m_eExplorationState = RANDOM_WALK;
-		m_fWheelSpeed = 10;
+		m_fWheelSpeed = 12;
 		m_fProximityThreshold = 0.05;
 		m_bLocked = false;
 		std::map<std::string, Real>::iterator it = m_mapParameters.find("rwm");
