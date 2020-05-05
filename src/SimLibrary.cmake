a440e41e3367a60b3aab4e80e1d0664e28141d9a
Ken <ken@kenh.fr>
2020-05-05 17:48:35 +0200

    relative RPATH for libautomode enhance portability

--- src/SimLibrary.cmake
+++ src/SimLibrary.cmake
@@ -62,6 +62,8 @@ target_link_libraries(automode argos3plugin_${ARGOS_BUILD_FOR}_epuck)
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
 add_executable(automode_main AutoMoDeMain.cpp)
+# set_target_properties(automode_main PROPERTIES BUILD_RPATH_USE_ORIGIN TRUE )
+set_target_properties(automode_main PROPERTIES BUILD_RPATH "$ORIGIN/../build/src")
 target_link_libraries(automode_main automode argos3core_${ARGOS_BUILD_FOR} argos3plugin_${ARGOS_BUILD_FOR}_epuck argos3_demiurge_loop_functions argos3_demiurge_epuck_dao)
 
 # add_executable(visualize_fsm AutoMoDeVisualizeFSM.cpp)
